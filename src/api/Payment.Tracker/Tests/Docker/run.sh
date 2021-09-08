57e0615e66812e565ed9d81d91555ea0e410544f
Bartek Zawadka <kontakt@bartoszzawadka.pl>
2021-09-08 21:17:29 +0200

    Update run.sh

--- src/api/Payment.Tracker/Tests/Docker/run.sh
+++ src/api/Payment.Tracker/Tests/Docker/run.sh
@@ -4,4 +4,4 @@ set -e
 docker-compose -f $BUILD_SOURCESDIRECTORY/src/api/Payment.Tracker/Tests/Docker/docker-compose.yaml -p bartekzawadka/payment-tracker rm
 docker-compose -f $BUILD_SOURCESDIRECTORY/src/api/Payment.Tracker/Tests/Docker/docker-compose.yaml -p bartekzawadka/payment-tracker build
 docker-compose -f $BUILD_SOURCESDIRECTORY/src/api/Payment.Tracker/Tests/Docker/docker-compose.yaml -p bartekzawadka/payment-tracker up --force-recreate --abort-on-container-exit
-docker cp payment-tracker-integration-tests:/app/Tests/Payment.Tracker.IntegrationTests/TestResults/IntegrationTestResults.trx $BUILD_SOURCESDIRECTORY
+docker cp payment-tracker-integration-tests:/app/Tests/Payment.Tracker.IntegrationTests/bin/Release/5.0/TestResults/IntegrationTestResults.trx $BUILD_SOURCESDIRECTORY
